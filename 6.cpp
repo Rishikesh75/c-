50d60fb1e5dc476bff16f7de0a91ad1c4c30c02d
Rishikesh <kotlarishikesh27@gmail.com>
2024-05-23 11:15:18 +0530

    strucutres

---
+++ 6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+struct student 
+{
+    int x;
+    char name[20];
+};
+void display(struct student s1)
+{
+    cout<<"RollNo:"<<s1.x<<endl;
+    cout<<"Name:"<<s1.name<<endl;
+}
+int main()
+{
+struct student s1;
+cout<<"Enter the roll number of the student"<<endl;
+cin>>s1.x;
+cout<<"Enter the name of the student"<<endl;
+cin>>s1.name;
+display(s1);
+return(0);
+}
